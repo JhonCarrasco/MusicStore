b79660f7c4d0f20767e9d68b3b02a40140a2d021
Jhon Carrasco <jhon.carrasco@gmail.com>
2024-07-23 15:21:28 -0400

    feat: modulo 1

--- MusicStore.Api/Program.cs
+++ MusicStore.Api/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Persistence;
 using MusicStore.Repositories;
+using MusicStore.Repositories.Implements;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Configuring Context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"));
+});
+
 //Registering services
-builder.Services.AddScoped<GenreRepository>();
+builder.Services.AddTransient<IGenreRepository, GenreRepository>();
 
 var app = builder.Build();
 
---
+++ MusicStore.Repositories/IGenreRepository.cs
@@ -0,0 +1,14 @@
+﻿using MusicStore.Dto.Request;
+using MusicStore.Dto.Response;
+
+namespace MusicStore.Repositories
+{
+    public interface IGenreRepository
+    {
+        Task<int> AddAsync(GenreRequestDTO genre);
+        Task DeleteAsync(int id);
+        Task<List<GenreResponseDTO>> GetAsync();
+        Task<GenreResponseDTO?> GetAsync(int id);
+        Task UpdateAsync(int id, GenreRequestDTO genre);
+    }
+}
